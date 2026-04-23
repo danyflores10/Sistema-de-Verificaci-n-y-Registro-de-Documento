4889b23ebc4e0e84607b53258c43a8cba0b9ddd5
danyflores10 <130566439+danyflores10@users.noreply.github.com>
2026-04-23 12:23:45 -0400

    cambios eliminados

--- storage/framework/views/3ea5160ca37469e4dc4c4f38dd9cbb74.php
+++
@@ -1,5 +0,0 @@
-<table <?php echo e($attributes->merge(['class' => 'w-full'])); ?>>
-    <?php echo e($slot); ?>
-
-</table>
-<?php /**PATH C:\Users\hp pavilion\OneDrive\Escritorio\System Correos\system-correos\vendor\laravel\pulse\resources\views\components\table.blade.php ENDPATH**/ ?>
--- storage/framework/views/886c06fed14164df2a9a6ca8ac474a81.php
+++
@@ -1,5 +0,0 @@
-<a <?php echo e($attributes->merge(['class' => 'block w-full px-4 py-2 text-start text-sm leading-5 transition duration-150 ease-in-out focus:outline-none', 'style' => 'color: var(--text-secondary);'])); ?>
-
-   onmouseover="this.style.backgroundColor='var(--surface-border)'"
-   onmouseout="this.style.backgroundColor='transparent'"><?php echo e($slot); ?></a>
-<?php /**PATH C:\Users\hp pavilion\OneDrive\Escritorio\System Correos\system-correos\resources\views/components/dropdown-link.blade.php ENDPATH**/ ?>
